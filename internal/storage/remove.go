3419777bee835f4c3db633230cec81f5cdb2c26a
Andriy Massimilla <work@andriym.dev>
2023-08-04 12:15:33 -0400

    Implement remove command

---
+++ internal/storage/remove.go
@@ -0,0 +1,53 @@
+package storage
+
+import (
+	"dvs/internal/config"
+	"dvs/internal/git"
+	"dvs/internal/meta"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/exp/slog"
+)
+
+// Remove a file from storage
+func Remove(path string, conf config.Config, gitDir string, dry bool) error {
+	/// Get metadata of specified file
+	metadata, err := meta.LoadFile(path)
+	if err != nil {
+		return err
+	}
+
+	// Get storage path
+	storagePath := filepath.Join(conf.StorageDir, metadata.FileHash) + FileExtension
+
+	// Remove file from storage
+	if !dry {
+		err = os.Remove(storagePath)
+		if err != nil {
+			return err
+		}
+		slog.Info("Removed file from storage", slog.String("path", storagePath))
+	} else {
+		slog.Info("Dry run: removed file from storage", slog.String("path", storagePath))
+	}
+
+	// Remove path from gitignore
+	err = git.RemoveIgnoreEntry(gitDir, path, dry)
+	if err != nil {
+		return err
+	}
+
+	// Remove metadata file
+	if !dry {
+		err = os.Remove(path + meta.FileExtension)
+		if err != nil {
+			slog.Error("Failed to remove metadata file", slog.String("path", path))
+		}
+		slog.Debug("Removed metadata file", slog.String("path", path))
+	} else {
+		slog.Debug("Dry run: removed metadata file", slog.String("path", path))
+	}
+
+	return nil
+}
