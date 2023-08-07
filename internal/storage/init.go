ea62b0a6a98bde034de039b05247d792932bf910
Andriy Massimilla <work@andriym.dev>
2023-08-07 12:31:03 -0400

    Better check logic in init

--- internal/storage/init.go
+++ internal/storage/init.go
@@ -24,38 +24,32 @@ func Init(storageDir string) error {
 	if err != nil {
 		slog.Info("Creating storage directory", slog.String("path", storageDir), slog.String("permissions", defaultDirPermissions.String()))
 
-		// Create storage dir
+		// Create storage dir and necessary parents
 		err = os.MkdirAll(storageDir, defaultDirPermissions)
 		if err != nil {
 			slog.Error("Failed to create storage directory", slog.String("path", storageDir))
 			return err
 		}
-		fileInfo, err = os.Stat(storageDir)
-		if err != nil {
+	} else {
+		// Ensure destination is a directory
+		if !fileInfo.IsDir() {
+			slog.Error("Destination isn't a directory", slog.String("path", storageDir))
 			return err
 		}
-	}
 
-	// Ensure destination is a directory
-	if !fileInfo.IsDir() {
-		slog.Error("Destination is not a directory", slog.String("path", storageDir))
-		return err
-	}
+		// Ensure destination has write permissions
+		if fileInfo.Mode().Perm()&0200 == 0 {
+			slog.Error("Destination doesn't have write permissions", slog.String("path", storageDir), slog.String("permissions", fileInfo.Mode().Perm().String()))
+			return os.ErrPermission
+		}
 
-	// Ensure storage dir has write permissions
-	if fileInfo.Mode().Perm()&0200 == 0 {
-		slog.Error("Destination does not have write permissions", slog.String("path", storageDir))
-		return os.ErrPermission
-	}
-
-	// Warn if not empty
-	dir, err := os.ReadDir(storageDir)
-	if err != nil {
-		slog.Error("Failed to read storage directory", slog.String("path", storageDir))
-		return err
-	}
-	if len(dir) > 0 {
-		slog.Warn("Storage directory isn't empty", slog.String("path", storageDir))
+		// Warn if destination is not empty
+		dir, err := os.ReadDir(storageDir)
+		if err != nil {
+			slog.Error("Failed to read storage directory", slog.String("path", storageDir))
+		} else if len(dir) > 0 {
+			slog.Warn("Storage directory isn't empty", slog.String("path", storageDir))
+		}
 	}
 
 	// Get repository root
