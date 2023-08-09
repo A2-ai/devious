0556500fbe2c4f6da797feaa9a1337f088acd443
Andriy Massimilla <work@andriym.dev>
2023-08-09 15:09:01 -0400

    Disable init no perms test

--- internal/storage/init_test.go
+++ internal/storage/init_test.go
@@ -6,34 +6,35 @@ import (
 	"testing"
 )
 
-func TestInitNoPerms(t *testing.T) {
-	// Create temp directory
-	tempDir, err := os.MkdirTemp(".", "temp")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		err = os.RemoveAll(tempDir)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}()
+// kind of hard to check for perms without writing a file, instead let's just check perms when trying to write a file
+// func TestInitNoPerms(t *testing.T) {
+// 	// Create temp directory
+// 	tempDir, err := os.MkdirTemp(".", "temp")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	defer func() {
+// 		err = os.RemoveAll(tempDir)
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+// 	}()
 
-	// Create storage directory with limited permissions
-	err = os.Mkdir(filepath.Join(tempDir, "storage"), 0000)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	// Create storage directory with limited permissions
+// 	err = os.Mkdir(filepath.Join(tempDir, "storage"), 0000)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	// Run function
-	err = Init(
-		tempDir,
-		filepath.Join(tempDir, "storage"),
-	)
-	if err == nil {
-		t.Error("Function did not return error")
-	}
-}
+// 	// Run function
+// 	err = Init(
+// 		tempDir,
+// 		filepath.Join(tempDir, "storage"),
+// 	)
+// 	if err == nil {
+// 		t.Error("Function did not return error")
+// 	}
+// }
 
 func TestInitNoParent(t *testing.T) {
 	// Create temp directory
