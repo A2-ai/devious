dcd370983bf25d4b625353ca2b42459a77a62a08
Andriy Massimilla <work@andriym.dev>
2023-08-15 14:02:42 -0400

    Add default URL for install script

--- scripts/install_user.sh
+++ scripts/install_user.sh
@@ -2,10 +2,17 @@
 
 INSTALL_DIR=.local/bin
 
+# If no URL is provided, use the default
+INSTALL_URL=$1
+if [ -z "$INSTALL_URL" ]; then
+    echo "No URL provided, using default"
+    INSTALL_URL=https://github.com/A2-ai/devious/releases/latest/download/dvs_Linux_x86_64.tar.gz
+fi
+
 mkdir ~/$INSTALL_DIR
 cd ~/$INSTALL_DIR
 echo 'export PATH="$HOME/'$INSTALL_DIR':$PATH"' | tee -a ~/.bashrc ~/.profile
-wget $1 -O dvs.tar.gz
+wget $INSTALL_URL -O dvs.tar.gz
 tar -xzf dvs.tar.gz dvs
 chmod +x dvs
 rm dvs.tar.gz
