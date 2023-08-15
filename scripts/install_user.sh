b81df729557f64a9f702288a45e8e05bf7952a1b
Andriy Massimilla <work@andriym.dev>
2023-08-15 15:39:00 -0400

    Don't add to PATH if bin directory already exists

--- scripts/install_user.sh
+++ scripts/install_user.sh
@@ -9,9 +9,11 @@ if [ -z "$INSTALL_URL" ]; then
     INSTALL_URL=https://github.com/A2-ai/devious/releases/latest/download/dvs_Linux_x86_64.tar.gz
 fi
 
-mkdir ~/$INSTALL_DIR
+# Create the user bin directory and add it to the PATH if it doesn't exist
+mkdir ~/$INSTALL_DIR && echo 'export PATH="$HOME/'$INSTALL_DIR':$PATH"' | tee -a ~/.bashrc ~/.profile
+
+# Download and extract the binary
 cd ~/$INSTALL_DIR
-echo 'export PATH="$HOME/'$INSTALL_DIR':$PATH"' | tee -a ~/.bashrc ~/.profile
 wget $INSTALL_URL -O dvs.tar.gz
 tar -xzf dvs.tar.gz dvs
 chmod +x dvs
