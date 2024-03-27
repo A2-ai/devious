c32b1c2f14d490135f0e5a6eedc6ea7c7446e2eb
Jenna Elwing <jenna@a2-ai.com>
2024-03-27 11:28:54 -0400

    purging debug files

---
+++ src/internal/storage/get.rs
@@ -0,0 +1,10 @@
+use std::path::PathBuf;
+
+use crate::internal::file::hash;
+use crate::internal::meta;
+use crate::internal::utils::utils;
+
+// pub fn get(local_path: &PathBuf, storage_dir: &PathBuf, git_dir: &PathBuf) -> Result<(), std::io::Error> {
+//     // get metadata
+    
+// } // get
